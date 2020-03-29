50d0e24bf9f16a8efbccb2c6e68e8b2f213259fa
neebjames <neebjames@gmail.com>
2020-03-29 12:06:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    TextField,
+    Button
+  } from 'react-md';
+
+  
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+}
+
+callAPI() {
+    fetch("http://localhost:4000/familydetails")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
+    render() {
+        return (
+            <div className="md-grid">
+           
+    <TextField id="placeholder-only-title" placeholder="Enter house name" className="md-cell md-cell--bottom" block={true}/>
+    <Button floating>home</Button>
+    <p className="App-intro">{this.state.apiResponse}</p>
+
+          </div>
+        );
+      }
+}
